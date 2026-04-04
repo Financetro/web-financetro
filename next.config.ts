d30114dafc786b49d1976104cd0dc9c69331dce6
LAPTOP-U0QC1G5U\Kapil <kapilkumawat966@gmail.com>
2026-04-04 20:44:44 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/firebase-messaging-sw.js',
+        destination: '/api/sw',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
