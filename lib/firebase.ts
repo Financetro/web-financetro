afb48bc262f6cba76b187eedfc701a2ca2ca6ff3
LAPTOP-U0QC1G5U\Kapil <kapilkumawat966@gmail.com>
2026-04-04 20:43:43 +0530

    update

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getMessaging, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzvVYmzkaB4jSW1uFg3z1-5pznlhMBAik",
-  authDomain: "financetro.firebaseapp.com",
-  databaseURL: "https://financetro-default-rtdb.firebaseio.com",
-  projectId: "financetro",
-  storageBucket: "financetro.firebasestorage.app",
-  messagingSenderId: "662862194800",
-  appId: "1:662862194800:web:becdcf1b579dc0ad7d6cb5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -23,8 +23,8 @@ const storage = getStorage(app);
 
 // Messaging initialization done conditionally for SSR safety
 const messaging = async () => {
-    const supported = await isSupported();
-    return supported ? getMessaging(app) : null;
+  const supported = await isSupported();
+  return supported ? getMessaging(app) : null;
 };
 
 export { app, auth, db, storage, messaging };
